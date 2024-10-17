e674b24dc4680ba90fedf65ad1dcc506fc3f8a89
mico2911 <macollantes@avatar-global.com>
2024-10-17 12:16:08 -0500

    data proyecto

---
+++ models/entrada.js
@@ -0,0 +1,11 @@
+module.exports = class Entrada {
+
+    constructor(codigo, codEvento, codTipoEntrada, precio, cantidadVenta) {        
+        this.codigo = codigo;
+        this.codEvento = codEvento;
+        this.codTipoEntrada = codTipoEntrada;
+        this.precio = precio;
+        this.cantidadVenta = cantidadVenta;
+    }
+
+}
---
+++ models/tipoEntrada.js
@@ -0,0 +1,8 @@
+module.exports = class TipoEntrada {
+
+    constructor(codigo, nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+    
+}
