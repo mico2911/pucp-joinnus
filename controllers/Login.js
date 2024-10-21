57a157548a349f0a7bb58b14365877faa642b10c
miguelAvatr <macollantes@avatar-global.com>
2024-10-21 16:36:06 -0500

    login y register cambios

--- controllers/Login.js
+++ controllers/Login.js
@@ -1,1 +1,25 @@
 // Controlador para iniciar sesión
+const fs = require('fs');
+const path = require('path');
+const usuariosPath = path.join(__dirname, '..', 'data', 'usuarios.json');
+
+// Controlador para manejar el inicio de sesión
+exports.login = async (req, res) => {
+    const { correo, password } = req.body;
+
+    // Verificar si el usuario existe
+    const users = JSON.parse(fs.readFileSync(usuariosPath, 'utf8'));
+    const user = users.find(user => user.correo === correo);
+    if (!user) {
+        return res.status(401).send('Usuario no encontrado.');
+    }
+
+    // Verificar si la contraseña es correcta
+    const isPasswordCorrect = password == user.password;
+    if (!isPasswordCorrect) {
+        return res.status(401).send('Contraseña incorrecta.');
+    }
+
+    // Iniciar sesión exitoso
+    res.send(`Bienvenido, ${user.nombre}!`);
+};
--- routes/tienda.js
+++ routes/tienda.js
@@ -2,9 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require ('../controllers/Home');
+const accountController = require('../controllers/Account');
+const loginController = require('../controllers/Login');
 
 router.get ('/', homeController.getIndex);
 router.get ('/eventos', homeController.getEventos);
 router.get ('/eventos/:codigoEvento', homeController.getEvento);
+
+router.get('/register', (req, res) => {
+    res.render('tienda/register');
+});
+
+router.post('/register', accountController.register);
+
+router.get('/login', (req, res) => {
+    res.render('tienda/login');
+});
+
+router.post('/login', loginController.login);
+
+module.exports = router;
 
 module.exports = router;
